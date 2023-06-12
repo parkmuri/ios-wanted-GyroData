b768d929579727475d86fcfab5b3b64fb6b606ba
parkmuri <smxhmu@naver.com>
2023-06-12 16:49:51 +0900

    feat: 스토리보드 삭제 및 navigationController 설정

--- GyroData/ViewController.swift
+++ GyroData/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureRootView()
+        configureNavigationBar()
+    }
+    
+    private func configureRootView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func configureNavigationBar() {
+        title = "목록"
+        
+        let barButton = UIBarButtonItem(title: "측정", style: .plain, target: self, action: #selector(barButtonTapped))
+        
+        navigationItem.rightBarButtonItem = barButton
     }
 
+    @objc
+    private func barButtonTapped() {
+        
+    }
 
 }
 
